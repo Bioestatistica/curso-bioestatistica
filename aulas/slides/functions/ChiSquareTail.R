3ef82fe4168bbc054714b85ace7b9287eb7d3930
rafamaia <rpmaia@g.unicamp.br>
2017-06-27 15:51:40 -0300

    atualizando parte 10

--- aulas/slides/functions/ChiSquareTail.R
+++ aulas/slides/functions/ChiSquareTail.R
@@ -7,7 +7,7 @@ ChiSquareTail <- function(U, df, xlim = c(0, 10), col = fadeColor("black", "22")
   x <- c(0, seq(xlim[1], xlim[2] + 3, length.out = 300))
   y <- c(0, dchisq(x[-1], df))
   plot(x, y, type = 'l', axes = FALSE, xlim = xlim, lwd=2, 
-       main = bquote("DistribuiÃ§Ã£o "* chi[.(df)]^2), xlab="")
+       main = bquote("Distribuição "* chi[.(df)]^2), xlab="")
   abline(h = 0)
   if (axes) {
     axis(1)
